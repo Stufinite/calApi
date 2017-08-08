26b770fd8f7a658ddceb9a6c4eadac56a4c9e1f1
david <davidtnfsh@gmail.com>
2017-08-08 16:15:27 +0800

    write python manage.py migrate to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/Stufinite/CampassCrawler.git;
 pip install -r CampassCrawler/requirements.txt
 cd CampassCrawler/UCrawler; scrapy crawl NSYSU -o ../../calApi/NSYSU.json -t json; cd -
-cd calApi; pwd; python manage.py buildCourse  NSYSU.json NSYSU 1061
+cd calApi; pwd; python manage.py migrate; python manage.py buildCourse  NSYSU.json NSYSU 1061
